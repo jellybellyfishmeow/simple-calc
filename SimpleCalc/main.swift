242fbd5e6be137e557c1f8a527455898e3f4548e
Jingyu Yang <yangj9@uw.edu>
2017-10-12 10:58:23 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by iGuest on 10/12/17.
+//  Copyright © 2017 Jingyu Yang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
